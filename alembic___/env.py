3235ff8d29f46b24c71ae87d7104ce9a9dfd4916
Nickolay Mozganov <nick-mozg1@mail.ru>
2021-03-16 01:30:46 +0500

    alembic test

--- alembic___/env.py
+++ alembic___/env.py
@@ -7,17 +7,19 @@ from sqlalchemy import pool
 from alembic import context
 
 # for local migration:
-# if os.getenv('DEBUG', True):
-#     import sys
-#     import pathlib
-#
-#     to_replace = '\\'
-#     replace_by = '\\\\'
-#     path = pathlib.Path(__file__).parent.absolute()
-#     path = str(path).replace(to_replace, replace_by)[:-len('alembic')]
-#     sys.path.insert(0, path)
+if os.getenv('DEBUG', True):
+    import sys
+    import pathlib
+
+    to_replace = '\\'
+    replace_by = '\\\\'
+    path = pathlib.Path(__file__).parent.absolute()
+    path = str(path).replace(to_replace, replace_by)[:-len('alembic___')]
+    sys.path.insert(0, path)
 
 from data.db_session import SqlAlchemyBase
+from alembic.config import Config
+alembic_cfg = Config("alembic___.ini")
 import data.__all_models
 
 # this is the Alembic Config object, which provides
