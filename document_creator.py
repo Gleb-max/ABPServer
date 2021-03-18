9717aa8b5d3a676ac3eb5bfc05b7057bd7fd48d2
Nickolay Mozganov <nick-mozg1@mail.ru>
2021-03-18 14:25:51 +0500

    hotfix

--- document_creator.py
+++ document_creator.py
@@ -459,7 +459,6 @@ def create_attendance_log(file_name, users: List[User], group: StudentsGroup):
     table.rows[0].cells[0].merge(table.cell(1, 0))
     table.cell(0, 0).add_paragraph('№\nп/п')
     table.cell(1, 0).width = Cm(1.2)
-    table.cell(2, 0).width = Cm(1.2)
 
     table.rows[0].cells[1].merge(table.cell(1, 1))
 
